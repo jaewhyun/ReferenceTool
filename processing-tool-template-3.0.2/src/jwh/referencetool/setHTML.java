d142f85e7a7398122cf7838a9229d36f6c11df53
Jae Hyun <jaewhyun@gmail.com>
2018-08-02 17:03:39 -0700

    fixing bug for arc

--- processing-tool-template-3.0.2/src/jwh/referencetool/setHTML.java
+++ processing-tool-template-3.0.2/src/jwh/referencetool/setHTML.java
@@ -84,7 +84,8 @@ public class setHTML extends JEditorPane {
 					|| searchText.equals("me")
 					|| searchText.equals("he")
 					|| searchText.equals("she")
-					|| searchText.equals("had")) {
+					|| searchText.equals("had")
+					|| searchText.equals("arc")) {
 				searchText = " " + searchText + " ";
 			}
 			
